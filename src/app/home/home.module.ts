2c6adebe5451ac414a9b7f04c7657c99099855eb
Rafael de Carvalho Andriollo <rafael.andriollo@fatec.sp.gov.br>
2021-11-05 16:35:32 -0300

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [QRScanner]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,71 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  scanner: any;
+  content: HTMLElement;
+  img: HTMLElement;
+
+  resultado = '';
+
+  constructor(
+    private qrScanner: QRScanner,
+    public alertController: AlertController,
+    private platform: Platform
+  ) {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      this.content.style.opacity = '1';
+
+      this.resultado = '';
+
+      this.qrScanner.hide(); // hide camera preview
+      this.scanner.unsubscribe(); // stop scanning
+    });
+
+  }
+
+  lerQRCode() {
+    this.content = document.getElementsByTagName('ion-content')[0];
+
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+          this.content.style.opacity = '0';
+
+          // start scanning
+          this.scanner = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+
+            this.resultado = (text['result']) ? text['result'] : text;
+            this.content.style.opacity = '1';
+
+            this.exibirAlerta(this.resultado);
+
+            this.qrScanner.hide(); // hide camera preview
+            this.scanner.unsubscribe(); // stop scanning
+          });
+
+        } else if (status.denied) {
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+
+  }
+
+  async exibirAlerta(mensagem: string) {
+    const alert = await this.alertController.create({
+      header: 'QRScanner',
+      message: mensagem,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+  }
 
 }
